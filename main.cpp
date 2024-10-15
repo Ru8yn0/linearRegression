19c48cc09d8b8758d8387f17fcbd88ece6d0d70c
Singh Manveer <163578431+Ru8yn0@users.noreply.github.com>
2024-10-15 16:14:37 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,78 @@
+//
+//  main.cpp
+//  LinearRegression
+//
+//  Created by Surinder pal Singh on 13/10/24.
+// f(x) = y;
+// w = (xi - x)(yi-y)/(xi-x)^2
+// b = wx-MediaY
+// y = wx+b
+
+#include <iostream>
+#include <cmath>
+int Weight(int Grandezze[3],int pesi[3],float& weight,float& mediaY,int& mediaX){
+    
+    float calcoloTotX,calcoloTotY,calcoloTot;
+    float sommaX, sommaY = 0;
+    //x
+    for(int i = 0; i < 3;i++){
+        sommaX+= Grandezze[i];
+        mediaX = (sommaX)/3;
+
+    }
+    //y
+    for(int i = 0; i < 3;i++){
+        sommaY += pesi[i];
+        mediaY = (sommaY)/3;
+    }
+    //(xi-x)(yi-y)
+    int abc = 0;
+    for(int i = 0; i <= 2; i++){
+        calcoloTotX = (Grandezze[i]-mediaX);
+        calcoloTotY = (pesi[i]-mediaY);
+
+        calcoloTot += calcoloTotX*calcoloTotY;
+        abc+=calcoloTotX*calcoloTotX;
+    }
+    //risultato
+    weight = calcoloTot/abc;
+    return weight;
+}
+
+
+float bias(int Grandezze[3],int pesi[3]){
+    float weight;
+    float a,b;
+    float mediaY;
+    int mediaX;
+    float bias = 0;
+    Weight(Grandezze, pesi, weight,mediaY,mediaX);
+    bias = mediaY - (weight*mediaX);
+    return bias;
+}
+
+
+
+int main(int argc, const char * argv[]) {
+    int Grandezze[3] = {1,2,3};
+    int pesi[3] = {2,3,5};
+    int dataSum = 0;
+    
+    float weight,mediaY,b;
+    int mediaX;
+    Weight(Grandezze, pesi, weight, mediaY, mediaX);
+    b = bias(Grandezze, pesi);
+    int x;
+    float DataTest = 0;
+    float MSE = 0;
+    for(int i = 0; i < 3; i++){
+        DataTest = weight*Grandezze[i]+b;
+        MSE += pow(pesi[i]-DataTest, 2);
+    }
+    
+    MSE = MSE/3;
+    std::cout << "inserisci grandezza" << std::endl;
+    std::cin>> x;
+    std::cout << weight*x+b << std::endl;
+    std::cout << "errore medio: " << MSE << std::endl;
+}
